5e125cb052d42a3e8c2c0bb68fdba27608b5d2d9
Gopi <gopimec22@gmail.com>
2018-01-03 23:13:13 -0800

    Spark TestProject

---
+++ SparkTestProjects/build.sbt
@@ -0,0 +1,8 @@
+name := "SparkTestProjects"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
---
+++ SparkTestProjects/src/main/scala/StartSparkProcess.scala
@@ -0,0 +1,9 @@
+
+
+object StartSparkProcess {
+  def main(args: Array[String]): Unit = {
+    val sc = SparkSession.builder().master("local").appName("test").getOrCreate()
+    val rdd = sc.sparkContext.wholeTextFiles("C:\\Users\\welcome\\Documents\\Test")
+    rdd.foreach(println(_))
+  }
+}
